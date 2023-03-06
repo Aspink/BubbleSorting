e6437084cfc367910567d9861ac06f126cf38054
Aspink <aspink1@wp.pl>
2023-03-06 21:27:18 +0100

    some stuff added in Main class

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         int[] unsortedNumbers = new int[]{1, 2, 5, 3, 1, 7, 9, 1, 12, 83, 1, 5, 3, 2};
         printTable(unsortedNumbers);
+        printTable(Sorter.bubbleSorting(unsortedNumbers));
+        printTable(unsortedNumbers);
 
     }
     public static void printTable(int[] table) {
