014817da686e8aa09c3dd3d28c2b10aa47a98047
Aspink <aspink1@wp.pl>
2023-03-06 21:16:49 +0100

    printTable method added in Main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] unsortedNumbers = new int[]{1, 2, 5, 3, 1, 7, 9, 1, 12, 83, 1, 5, 3, 2};
+        printTable(unsortedNumbers);
+
+    }
+    public static void printTable(int[] table) {
+        System.out.print("{");
+        for(int number : table) {
+            System.out.print(number + ", ");
+        }
+        System.out.println("\b}");
     }
 }
